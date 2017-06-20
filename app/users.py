f2a4f3e112cb09933544dbd82096b84f86ba3741
Alexey Shevchenko <otetz@me.com>
2017-06-21 01:21:54 +0300

    Забыл подсказки типов.

--- app/users.py
+++ app/users.py
@@ -1,9 +1,9 @@
-from typing import NamedTuple
+from typing import NamedTuple, List, Dict, Any
 
 User = NamedTuple('User', [('entityid', int), ('userid', int), ('name', str)])
 
 
-def get_users(conn):
+def get_users(conn) -> List[Dict[str, Any]]:
     cur = conn.cursor()
     cur.execute("SELECT entityid, userid, name FROM users;")
     # noinspection PyProtectedMember,PyCallingNonCallable
